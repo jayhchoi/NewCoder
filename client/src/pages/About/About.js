070bba3313ec72de4b983affeac0bd3a941114a4
JayHChoi <jaychoi1619@gmail.com>
2019-03-16 02:15:52 +0900

    Implement markdown API for About page

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,34 +1,57 @@
 import React, { Component } from 'react'
 import ReactMarkdown from 'react-markdown'
 
-import about from './about.md'
 import Page from '../../wrappers/Page'
 
 class About extends Component {
   state = {
-    about: ''
+    posts: [],
+    isFetching: true
   }
 
   componentDidMount() {
-    fetch(about)
-      .then(res => res.text())
-      .then(text => this.setState({ about: text }))
+    fetch(
+      `https://api.github.com/repos/jayhchoi/newcoder-markdowns/contents/about`
+    )
+      .then(res => res.json())
+      .then(data => {
+        Promise.all(
+          data.map(item =>
+            fetch(
+              `https://raw.githubusercontent.com/jayhchoi/newcoder-markdowns/master/about/${
+                item.name
+              }`
+            )
+          )
+        )
+          .then(responses => Promise.all(responses.map(res => res.text())))
+          .then(data => this.setState({ posts: data, isFetching: false }))
+      })
       .catch(err => console.log(err))
+  }
+
+  renderPosts() {
+    if (this.state.isFetching) return <p>불러오는 중입니다...</p>
+
+    return this.state.posts.map((post, index) => {
+      return (
+        <div key={index} className="mb-4">
+          <ReactMarkdown source={post} />
+        </div>
+      )
+    })
   }
 
   render() {
     return (
-      <Page>
+      <Page className="bg-light">
         <div className="container">
           <div className="row">
             <div className="col-md-9 mx-auto">
               <h1 className="display-4">
                 <i className="fas fa-bullhorn" /> 공지사항
               </h1>
-              <hr />
-              <div>
-                <ReactMarkdown source={this.state.about} />
-              </div>
+              <div className="py-4">{this.renderPosts()}</div>
             </div>
           </div>
         </div>
